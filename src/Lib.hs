363d0c6909e04e386dea0e37af656be5d60318fb
Sam Ezeh <sam.z.ezeh@gmail.com>
2021-12-31 07:25:38 +0000

    Add tests, Add utilities, implement proof search, add (form) inference rule, make types first-class expressions

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,16 +2,14 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE PatternSynonyms #-}
 
-module Lib
-    ( Symbol
-    ) where
+module Lib where
 
 -- Basic Symbol declarations
 newtype Symbol = Symbol String deriving Eq
 newtype TypeIdentifier = TypeIdentifier Integer deriving Eq
 
 -- Variable definition
-newtype Variable = Variable Symbol deriving Eq -- TODO: Maybe variables should be indexed by integers
+data Variable = Variable Symbol | ArbitraryVariable deriving Eq -- TODO: Maybe variables should be indexed by integers
 
 -- Definition of types
 data SimpleType where
@@ -24,20 +22,16 @@ data Kind where
   KindArrow :: Kind -> Kind -> Kind
   deriving Eq
 
-data TypeOfKind where
-  TypeOfKindType :: TypeOfKind
-
 data InhabitableType where
   Type :: SimpleType -> InhabitableType
   Kind :: Kind -> InhabitableType
-  TypeOfKind :: InhabitableType
   Sort :: InhabitableType
+  ProductType :: Expression -> Expression -> InhabitableType
   deriving Eq
 
 data Value
 
 data TypeArrow a b = HLTypeArrow a b
-type (-->) = Entails
 
 -- Expression
 
@@ -73,48 +67,93 @@ type (:::) = Judgement
 pattern (:::) :: a -> b -> Judgement a b
 pattern  a ::: b = Judgement a b
 
-type TrueJudgement = Judgement Expression InhabitableType
+type TrueJudgement = Judgement Expression Expression
 type Entailment = Context :- TrueJudgement
 
+data a :- b = a :- b deriving Eq
+data Judgement a b = Judgement a b deriving Eq
+
 -- Constructor Synonyms
--- (:-) = Entails
 entails :: a -> b -> a :- b
 entails = (:-)
 
 pattern (:+:) :: Context -> Context -> Context
 pattern  a :+: b = Join a b
 
-
--- Entailment Definition
--- As a note, the program deals only with the type Entails Context (Judgement Expression InhabitableType)
-data a :- b = a :- b
-data Judgement a b = Judgement a b deriving Eq
-
+pattern (:-->) :: Expression -> Expression -> InhabitableType
+pattern a :--> b = ProductType a b
 
 -- Utility functions
 retreiveType :: Expression -> InhabitableType
 retreiveType (TypeExpression t) = t
-retreiveType _ = undefined 
+retreiveType _ = undefined
 
+{- TODO: Check this actually works as an arbitrary variable -}
 arbitraryVariable :: Expression
-arbitraryVariable = VariableExpression (Variable (Symbol "x"))
--- TODO: Check this actually works as an arbitrary variable
+arbitraryVariable = VariableExpression ArbitraryVariable -- (Variable (Symbol "x"))
+
+{-
+ - Introduces an arbitrary variable with a given type into a context
+ - 
+-}
+
+pattern Enhance :: Context -> Expression -> Context
+pattern Enhance ctx t = ctx :+: Context {declarations = [VariableExpression ArbitraryVariable ::: t]}
 
 eq3 :: Eq a => a -> a -> a -> Bool
 eq3 a b c = a == b && b == c
+
+eq4 :: Eq a => a-> a -> a -> a -> Bool
+eq4 a b c d = a == b && b == c && c == d
+
+emptyContext :: Context
+emptyContext = Context {declarations = []}
+
+
+pop :: Context -> TrueJudgement
+pop Context {declarations = declarations} = head declarations
+pop (Join left right) = pop right
+
+popEntailment :: Context -> Entailment
+popEntailment ctx = ctx :- pop ctx
+
+materialise :: Context -> Expression
+materialise ctx = case pop ctx of
+  VariableExpression ArbitraryVariable ::: t -> VariableExpression ArbitraryVariable
+  _ -> undefined 
+
+
+{-
+ -
+ - Searches for proof of statement in Context
+ -
+ -}
+find :: Context -> TrueJudgement -> Bool
+find Context {declarations = declarations} judgement = judgement `elem` declarations
+find (Join left right) judgement = find left judgement || find right judgement
 
 -- Inference Rules
 type InferenceRule = Context -> [Entailment] -> Entailment
 
 kindIsASort :: InferenceRule
-kindIsASort ctx _ = ctx :- Judgement (TypeExpression (Kind KindType)) Sort
+kindIsASort ctx _ = ctx :- Judgement (TypeExpression (Kind KindType)) (TypeExpression Sort)
 
-typeOfKindIsASort :: InferenceRule
-typeOfKindIsASort ctx _ = ctx :- Judgement (TypeExpression TypeOfKind) Sort
+sortIsASort :: InferenceRule
+sortIsASort ctx _ = ctx :- Judgement (TypeExpression Sort) (TypeExpression Sort)
+
+isSort :: Context -> Expression -> Bool
+isSort _ (TypeExpression t) = isSort' t
+isSort ctx variable@(VariableExpression _) = find ctx (variable ::: TypeExpression Sort)
+isSort _ _ = False
+
+isSort' :: InhabitableType -> Bool
+isSort' (Kind KindType) = True
+isSort' Sort = True
+isSort' _ = False
 
 -- (sort)
 sort :: InferenceRule
-sort ctx _ = ctx :- Judgement (TypeExpression (Kind KindType)) TypeOfKind
+sort ctx _ = ctx :- Judgement (TypeExpression (Kind KindType)) (TypeExpression Sort)
 
 -- (var)
 {-
@@ -122,10 +161,9 @@ sort ctx _ = ctx :- Judgement (TypeExpression (Kind KindType)) TypeOfKind
 - Then it's known that (x : A):ctx :- x : A
 -}
 var :: InferenceRule
-var ctx [gamma1 :- (a ::: b), gamma2 :- (c ::: Sort)] =
-  if eq3 ctx gamma1 gamma2 && TypeExpression b == c then
-    ctx :+: (Context {declarations = [arbitraryVariable ::: retreiveType a]})
-      :- (arbitraryVariable ::: retreiveType a)
+var ctx [gamma1 :- (a ::: b)] =
+  if ctx == gamma1 && isSort ctx b then
+      Enhance ctx a :- (arbitraryVariable ::: a)
   else
       undefined
 
@@ -133,15 +171,45 @@ var _ _ = undefined
 
 -- (weak)
 {-
-- If it's known that ctx :- (A : B) and ctx :- (C : s) then
-  (x : C) :- A : B
+- 
+- If it's known that ctx :- (A : B) and ctx :- (C : s),
+- Then (x : C) :- A : B
 - 
 -}
 weak :: InferenceRule
-weak ctx [gamma1 :- (a ::: b), gamma2 :- (c ::: Sort)] = 
+weak ctx [gamma1 :- (a ::: b), gamma2 :- (c ::: TypeExpression Sort)] =
   if eq3 ctx gamma1 gamma2 then
-    ctx :+: (Context {declarations = [arbitraryVariable ::: retreiveType c]})
-      :- (a ::: b)
+    Enhance ctx c :- (a ::: b)
   else
     undefined
-weak _ _ = undefined 
+weak _ _ = undefined
+
+
+-- (conv)
+{- 
+ - Beta reduction/conversion rule for types
+ -
+ - If it's know that ctx :- (a :: b) and b' is a sort
+ - Then if, b == b' then a :: b'
+ -
+ -}
+conv :: InferenceRule
+conv ctx [gamma1 :- (a ::: b), gamma2 :- (b' ::: c)] =
+  if eq3 ctx gamma1 gamma2 && b == b' && isSort ctx c then
+    ctx :- (a ::: b')
+  else undefined
+  where b'' = retreiveType b'
+conv _ _ = undefined
+
+-- (form)
+{-
+ - If it's known that A : * and (x : A):ctx :- B : s
+ - Then ctx :- (A --> B) : s
+ -}
+form :: InferenceRule
+form ctx [gamma1 :- (a ::: TypeExpression (Kind KindType)), (Enhance gamma2 a') :- (b ::: c)] =
+  if eq3 ctx gamma1 gamma2 && a == a' && isSort ctx c then
+    ctx :- (TypeExpression (a' :--> b) ::: c)
+  else
+    undefined
+form _ _ = undefined
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,32 @@
+{-# LANGUAGE BangPatterns #-}
+
+import Lib
+
+-- The following file should run without errors
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    let context = emptyContext
+    let !proof = productTypeIsASort context
+
+    putStrLn "Tests completed successfully"
+
+assertEqual :: Eq a => a -> a -> Bool
+assertEqual x y = (x == y) || error "Assertion failed"
+
+{- 
+ -
+ - We should be able to show that` 
+ - A --> * : TypeOfKind is deducible from A : *  
+ -
+ -}
+productTypeIsASort :: Context -> Entailment
+productTypeIsASort ctx = proofProductTypeIsASort
+    where
+        kindIsSort = sort ctx []
+        assumptionThatAIsKind = Enhance ctx (TypeExpression (Kind KindType))
+        aIsKind = var ctx [kindIsSort]
+        kindIsSort' = weak ctx [kindIsSort, aIsKind]
+        assumptionThatXHasTypeA = Enhance ctx (materialise assumptionThatAIsKind)
+        kindIsSort'' = weak  ctx [kindIsSort'', popEntailment assumptionThatXHasTypeA]
+        proofProductTypeIsASort = form ctx [aIsKind, kindIsSort'']
